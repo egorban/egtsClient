c65dad59d473299d032cbb3167524f6545a863ab
egorban <gorban@satissoft.ru>
2020-10-01 19:18:44 +0300

    fix deps

---
+++ cmd/egtsclient/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/egorban/ndtpClient/pkg/egtsclient"
+)
+
+func main() {
+	var serverAddress string
+	var numOIDs int
+	var time int
+	flag.StringVar(&serverAddress, "s", "localhost:9000", "server address (e.g. 'localhost:9000')")
+	flag.IntVar(&numOIDs, "i", 1, "number IDs (e.g. '1')")
+	flag.IntVar(&time, "t", 20, "time period seconds (e.g. '20')")
+	flag.Parse()
+	egtsclient.Start(serverAddress, numOIDs, time)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/egorban/ndtpClient
+
+go 1.12
+
+require (
+	github.com/ashirko/navprot v1.4.3
+	github.com/egorban/navprot v1.4.8
+)
